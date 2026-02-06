ccc81f15407a6c082a3ce6ecac406a68a0364c81
Udaiyaa-05 <udaiyaa.s2005@gmail.com>
2026-02-06 08:51:25 +0530

    3.11.9 updated

--- conditional.py
+++ conditional.py
@@ -2,14 +2,16 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import StrOutputParser
-from langchain_core.runnables import RunnableLambda,RunnableBranch
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnableBranch
 llm=ChatGroq(model="llama-3.1-8b-instant",temperature=0)
 parser=StrOutputParser()
 
-classifier_prompt=ChatPromptTemplate.from_messages([
-    {"system","answer only POSITIVE or NEGATIVE "},
-    {"human","{input}"}])
+classifier_prompt = ChatPromptTemplate.from_messages([
+    ("system", "answer only POSITIVE or NEGATIVE"),
+    ("human", "{input}"),
+])
 classifier_chain=classifier_prompt|llm|parser
 def clasify(text):
-    sentiment=classifier_chain.invoke({"text":text}.strip().upper())
+    sentiment = classifier_chain.invoke({"input": text.strip().upper()})
+    return sentiment
--- parallel_langchain.py
+++ parallel_langchain.py
@@ -8,15 +8,15 @@ from langchain_core.runnables import RunnableParallel
 llm = ChatGroq(model="llama-3.1-8b-instant", temperature=0)
 str_parser = StrOutputParser()
 
-good_news_prompt = ChatPromptTemplate.from_messages({
+good_news_prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a positive news generator."),
-    ("human", "Tell me some good news about BUSINESS.")
-})
+    ("human", "Tell me some good news about BUSINESS."),
+])
 
-bad_news_prompt = ChatPromptTemplate.from_messages({
+bad_news_prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a realistic news generator."),
-    ("human", "Tell me some bad news about BUSINESS.")
-})
+    ("human", "Tell me some bad news about BUSINESS."),
+])
 
 chain_good_news = good_news_prompt | llm | str_parser
 chain_bad_news = bad_news_prompt | llm | str_parser
